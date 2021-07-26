c1da99f459c9962865d227b70dfc65bbb8cf2124
Robocraft999 <alexakeil987@gmail.com>
2021-07-26 13:58:39 +0200

    added goggle diving helmet

---
+++ src/main/java/com/robocraft999/createplus/item/goggle/DivingGoggleArmor.java
@@ -0,0 +1,97 @@
+package com.robocraft999.createplus.item.goggle;
+
+import com.robocraft999.createplus.ClientEvents;
+import com.robocraft999.createplus.lists.ItemList;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.curiosities.armor.BackTankUtil;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.World;
+import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import top.theillusivec4.curios.api.CuriosApi;
+
+@EventBusSubscriber
+public class DivingGoggleArmor extends GoggleArmor {
+
+	public DivingGoggleArmor(IArmorMaterial material, Properties properties) {
+		super(material, properties);
+	}
+	
+	@SubscribeEvent(priority = EventPriority.LOW)
+	public static void breatheUnderwater(LivingUpdateEvent event) {
+		LivingEntity entity = event.getEntityLiving();
+		World world = entity.world;
+		boolean second = world.getGameTime() % 20 == 0;
+		boolean drowning = entity.getAir() == 0;
+
+		if (world.isRemote)
+			entity.getPersistentData()
+				.remove("VisualBacktankAirCP");
+		if (!(((GoggleArmor)ItemList.goggle_diving_helmet).isWornBy(entity)))
+			return;
+		if (!entity.areEyesInFluid(FluidTags.WATER))
+			return;
+		if (entity instanceof PlayerEntity && ((PlayerEntity) entity).isCreative())
+			return;
+
+		ItemStack backtank = getBackTank(entity);
+		if (backtank.isEmpty())
+			return;
+		if (!BackTankUtil.hasAirRemaining(backtank))
+			return;
+
+		if (drowning)
+			entity.setAir(10);
+
+		if (world.isRemote) {
+			entity.getPersistentData()
+				.putInt("VisualBacktankAirCP", (int) BackTankUtil.getAir(backtank));
+			//System.out.println(entity.getPersistentData().getInt("VisualBacktankAir"));
+			ClientEvents.timeLeft = (int) BackTankUtil.getAir(backtank);
+		}
+		
+		if (!second)
+			return;
+
+		entity.setAir(Math.min(entity.getMaxAir(), entity.getAir() + 10));
+		entity.addPotionEffect(new EffectInstance(Effects.WATER_BREATHING, 30, 0, true, false, true));
+		BackTankUtil.consumeAir(backtank, 1);
+	}
+	
+	private static ItemStack getBackTank(LivingEntity entity) {
+		ItemStack backtank = ItemStack.EMPTY;
+		
+		backtank = BackTankUtil.get(entity);
+		
+		if(backtank == ItemStack.EMPTY) {
+			Minecraft mc = Minecraft.getInstance();
+			ModLoadedCondition curiosloaded = new ModLoadedCondition("curios");
+			if(curiosloaded.test()) {
+				LazyOptional<IItemHandlerModifiable> test = CuriosApi.getCuriosHelper().getEquippedCurios(mc.player);
+				IItemHandlerModifiable test2 = test.orElse(null);
+				if(test2 != null) {
+					for(int i = 0; i < test2.getSlots();i++) {
+						ItemStack curiosSlot = test2.getStackInSlot(i);
+						if(curiosSlot.getItem() == AllItems.COPPER_BACKTANK.get())backtank = curiosSlot;
+					}
+				}
+			}
+		}
+		
+		return backtank;
+	}
+
+}
--- src/main/java/com/robocraft999/createplus/lists/ItemList.java
+++ src/main/java/com/robocraft999/createplus/lists/ItemList.java
@@ -1,8 +1,20 @@
 package com.robocraft999.createplus.lists;
 
+import java.util.ArrayList;
+
+import com.simibubi.create.AllItems;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class ItemList {
+	
+	public static ArrayList<Item> GOGGLES = new ArrayList<Item>();
 	
 	public static Item goggle_leather_helmet;
 	public static Item goggle_chainmail_helmet;
@@ -11,5 +23,49 @@ public class ItemList {
 	public static Item goggle_golden_helmet;
 	public static Item goggle_turtle_helmet;
 	public static Item goggle_netherite_helmet;
+	public static Item goggle_diving_helmet;
+	
+	public static void addGogglesToList() {
+		GOGGLES.add(goggle_leather_helmet);
+		GOGGLES.add(goggle_chainmail_helmet);
+		GOGGLES.add(goggle_iron_helmet);
+		GOGGLES.add(goggle_diamond_helmet);
+		GOGGLES.add(goggle_golden_helmet);
+		GOGGLES.add(goggle_turtle_helmet);
+		GOGGLES.add(goggle_netherite_helmet);
+		GOGGLES.add(goggle_diving_helmet);
+	}
+	
+	public static boolean isGoggleHelmet(ItemStack headSlot) {
+		Minecraft mc = Minecraft.getInstance();
+
+		boolean wearingGoggles = GOGGLES.contains(headSlot.getItem());
+		
+		ModLoadedCondition curiosloaded = new ModLoadedCondition("curios");
+		if(curiosloaded.test()) {
+			LazyOptional<IItemHandlerModifiable> test = CuriosApi.getCuriosHelper().getEquippedCurios(mc.player);
+			IItemHandlerModifiable test2 = test.orElse(null);
+			if(test2 != null) {
+				for(int i = 0; i < test2.getSlots();i++) {
+					ItemStack curiosSlot = test2.getStackInSlot(i);
+					if(curiosSlot.getItem() == AllItems.GOGGLES.get())wearingGoggles = true;
+				}
+			}
+		}
+		/*	
+		ModLoadedCondition mekloaded = new ModLoadedCondition("mekanism");
+		if(mekloaded.test()) {
+			if(headSlot.getItem() == MekanismItems.MEKASUIT_HELMET.asItem()) {
+				ItemMekaSuitArmor helmet = (ItemMekaSuitArmor)headSlot.getItem();
+				if(helmet.hasModule(headSlot, Modules.VISION_ENHANCEMENT_UNIT)) {//TODO add own Module
+					if(helmet.isModuleEnabled(headSlot, Modules.VISION_ENHANCEMENT_UNIT)) {//TODO add own Module
+						wearingGoggles = true;
+					}
+				}
+			}
+		}*/
+		
+		return wearingGoggles;
+	}
 	
 }
